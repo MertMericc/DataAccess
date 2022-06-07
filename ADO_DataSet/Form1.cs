53eb16c0c1e9fcf19a93f7c11d652d243b379723
MERT MERİÇ <mericasd2051@gmail.com>
2022-06-07 17:43:48 +0300

    Add project files.

---
+++ ADO_DataSet/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_DataSet
+{
+
+    public partial class Form1 : Form
+    {
+        string sqlconstr = "server=DESKTOP-NODMT1N;database=Northwind;integrated security=true;";
+        SqlConnection connection;
+        SqlCommand cmd;
+        DataTable tbl;
+        DataSet dsNorthwind;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "Select * from Customers";
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+
+            try
+            {
+                connection.Open();
+                tbl.Load(cmd.ExecuteReader());
+                dataGridView1.DataSource = tbl;
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(sqlconstr);
+            cmd = new SqlCommand();
+            dsNorthwind = new DataSet();
+            tbl = new DataTable();
+        }
+    }
+}
---
+++ Ado_Giris/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Ado_Giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SqlConnection veritabanina baglanmamizdan sorumlu class
+
+            string constrEv = "server=DESKTOP-NODMT1N;database=Northwind;integrated security=true;";
+            SqlConnection connection = new SqlConnection(constrEv);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "Select * from Shippers";
+
+            //2.yöntem
+            //SqlConnection con2 = new();
+            //con2.ConnectionString = constrEv;
+
+
+            try
+            {
+                connection.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine($"{rdr.GetInt32(0)}\t{rdr.GetString(1)}\t{rdr.GetString(2)}");
+                }
+
+                //2.yöntem
+                //while (rdr.Read())
+                //{
+                //    Console.WriteLine($"{rdr["ShipperId"]}\t{rdr["CompanyName"]}\t{rdr["Phone"]}");
+                //}
+
+
+            }
+            catch (SqlException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
